fe598e604a04f057615871d6caaab51912f0f483
AntonSeredenkov <seredenkov.anton2003@gmail.com>
2022-07-09 17:09:48 +0300

    project update

--- project/src/main/kotlin/com/example/refactor_vizualize_graph_ktln/Main.kt
+++ project/src/main/kotlin/com/example/refactor_vizualize_graph_ktln/Main.kt
@@ -10,29 +10,30 @@ import javafx.scene.Scene
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 
- class Main : Application() {
+class Main : Application() {
 
     var graph: Graph = Graph() //  граф
     override fun start(stage: Stage) {
-       /* var  root: BorderPane = BorderPane(); // создать сцену
-        root.center = graph.getScrollPane(); // разместить по центру холст вероятно
+        /* var  root: BorderPane = BorderPane(); // создать сцену
+         root.center = graph.getScrollPane(); // разместить по центру холст вероятно
 
-        var  scene: Scene = Scene(root, 1024.0, 768.0); // начальная размерность сцены
-        // scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("com.example.test_graph.application.css")).toExternalForm());
+         var  scene: Scene = Scene(root, 1024.0, 768.0); // начальная размерность сцены
+         // scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("com.example.test_graph.application.css")).toExternalForm());
 
-        stage.scene = scene;
-        stage.show(); // отобразить сцену
+         stage.scene = scene;
+         stage.show(); // отобразить сцену
 
-        addGraphComponents(); // создание графа
+         addGraphComponents(); // создание графа
 
-        var layout: Layout = RandomLayout(graph);
-        layout.execute();*/
+         var layout: Layout = RandomLayout(graph);
+         layout.execute();*/
         val fxmlLoader = FXMLLoader(Main::class.java.getResource("hello-view.fxml"))
         val scene = Scene(fxmlLoader.load(), 900.0, 600.0)
         stage.title = "Hello!"
         stage.scene = scene
         stage.show()
     }
+
     private fun addGraphComponents() {
         var model: Model = graph.getModel() // получить инструмент пользования
         graph.beginUpdate()
@@ -48,13 +49,13 @@ import javafx.stage.Stage
         model.addEdge("Cell A", "Cell B", 1);
         model.addEdge("Cell A", "Cell G", 2);
         model.addEdge("Cell A", "Cell F", 6);
-        model.addEdge("Cell B", "Cell C",10);
-        model.addEdge("Cell B", "Cell F",4);
-        model.addEdge("Cell B", "Cell G",5);
-        model.addEdge("Cell C", "Cell D",1);
-        model.addEdge("Cell D", "Cell E",4);
-        model.addEdge("Cell C", "Cell E",3);
-        model.addEdge("Cell E", "Cell F",10);
+        model.addEdge("Cell B", "Cell C", 10);
+        model.addEdge("Cell B", "Cell F", 4);
+        model.addEdge("Cell B", "Cell G", 5);
+        model.addEdge("Cell C", "Cell D", 1);
+        model.addEdge("Cell D", "Cell E", 4);
+        model.addEdge("Cell C", "Cell E", 3);
+        model.addEdge("Cell E", "Cell F", 10);
         model.addEdge("Cell F", "Cell G", 3);
 
         graph.endUpdate(); // добавление рёбер, вершин, очистка буфферов
@@ -62,8 +63,11 @@ import javafx.stage.Stage
         model.BoruvkaMST()
 
     }
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch(Main::class.java)
+        }
+    }
 }
 
-fun main() {
-    Application.launch(Main::class.java)
-}
---
+++ project/src/main/kotlin/com/example/refactor_vizualize_graph_ktln/SuperMain.kt
@@ -0,0 +1,10 @@
+package com.example.refactor_vizualize_graph_ktln
+
+class SuperMain {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Main.main(args);
+        }
+    }
+}
